7f153b80c5fa756e3f13352a41ad5063d69de43a
Pushkar Asodekar <pushkar9aso@gmail.com>
2024-03-14 22:51:56 +0530

    Final Program

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ import tcmain
 root = tkinter.Tk()
 root.title("Arcade Games")
 
-width = 900
-height = 700
+width = 910
+height = 450
+
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-x = (screen_width/2) - (width/2)
-y = (screen_height/2) - (height/2)
+
+x = (screen_width / 2) - (width / 2)
+y = (screen_height / 2) - (height / 2)
+
 root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 photo = ImageTk.PhotoImage(Image.open("icon.png"))
@@ -24,18 +27,35 @@ frame = ttk.Frame(root, padding=10)
 root.config(bg="#2F2F2F")
 
 def snake_call():
-    root.withdraw()
-    snakemain.snake_main(root)
+    """starts the snake game"""
+    try:
+        root.withdraw()
+        snakemain.snake_main(root)
+    except:
+        pass
 
 def pong_call():
-    root.withdraw()
-    pongmain.pong_main(root)
+    """starts the ping pong game"""
+    try:
+        root.withdraw()
+        pongmain.pong_main(root)
+    except:
+        pass
 
 def turtle_crossing_call():
-    root.withdraw()
-    tcmain.turtle_crossing_main(root)
+    """starts the turtle crossing game"""
+    try:
+        root.withdraw()
+        tcmain.turtle_crossing_main(root)
+    except:
+        pass
 
-arcade_games_label = ttk.Label(root, text="ARCADE GAMES", font=("Arial", 20, "bold"))
+
+arcade_games_label = ttk.Label(root, text="ARCADE GAMES", background='#2F2F2F', foreground='white', font=("Arial", 20, "bold"))
+arcade_games_label.grid(row=1000, column=20, padx=20, pady=20)
+
+choice_label = tkinter.Label(root, text="Choose the games you want to play", background='#2F2F2F', foreground='white', font=("Arial", 16, "bold"))
+choice_label.grid(row=1500, column=20, padx=20, pady=20)
 
 snake_image = Image.open("snake_image.png")
 ping_pong_image = Image.open("pingpong_image.png")
@@ -50,11 +70,11 @@ ping_pong_photo_image = ImageTk.PhotoImage(ping_pong_image)
 turtle_photo_image = ImageTk.PhotoImage(turtle_image)
 
 snake_button = Button(root, image=snake_photo_image, command=snake_call)
-snake_button.grid(row=1000, column=10, padx=20, pady=20)
+snake_button.grid(row=2000, column=10, padx=20, pady=20)
 
 pong_button = ttk.Button(root, image=ping_pong_photo_image, command=pong_call)
-pong_button.grid(row=1000, column=20, padx=20, pady=20)
+pong_button.grid(row=2000, column=20, padx=20, pady=20)
 
 turtle_crossing_button = ttk.Button(root, image=turtle_photo_image, command=turtle_crossing_call)
-turtle_crossing_button.grid(row=1000, column=30, padx=20, pady=20)
+turtle_crossing_button.grid(row=2000, column=30, padx=20, pady=20)
 root.mainloop()
